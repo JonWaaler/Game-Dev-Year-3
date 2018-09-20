bfd68a88fc29fd15b2e540076c37327f6ee50002
Jon Waaler <jon.waaler@uoit.net>
2018-09-20 15:25:28 -0400

    Prototype folder

---
+++ Prototype/Assets/Scripts/GunBehavior.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunBehavior : MonoBehaviour
+{
+
+    [Header("Gun Firing Options")]
+    public float RateOfFire = 0.5f;
+    public float t_RateOfFireTimer = 0.5f;  // Should keep the timer init to  the val of Rate
+    public float Recoil = 10f;              //  OfFire so you dont have to wait for the timer
+    public float Damage = 10f;
+    public float TimeToReload = 3f;
+    public int BulletsInMag = 15;       // The amount of bullets currently in mag
+    public int MagazineCapacity = 15;   // How many bullets the mag can hold
+
+
+    [Header("Particle Effects")]
+    public ParticleSystem Gun_Shot; //When a bullet is shot sparks
+    public ParticleSystem Gun_Smoke;// After a bullet is shot smoke, or when reloading...
+
+    [HideInInspector] // Bullet Pooling
+    public bool isShooting = false;
+
+    [Header("Bullet Pooling")]
+    public int BULLET_POOL_SIZE;        // How many bullets to pool for this weapon
+    public List<GameObject> Bullets;    // A "dynamic" array type for the bullets
+
+    [Header("Gun Set-Up")]
+    public GameObject Prefab_Bullet;    // This is a copy of our bullet
+    public Transform Emitter;           // Emitter is the spawn point of the bullet
+
+    private GameObject player;
+    bool requestReload = false;
+    public float t_Reload = 0;
+
+    public string RT_PNum;
+    public string xButton_PNum;
+
+    private string tempReloading_Str;
+
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        
+        for (int i = 0; i < BULLET_POOL_SIZE; i++)
+        {
+            // Spawn in a specified bullets
+            GameObject myInstance = Instantiate<GameObject>(Prefab_Bullet);
+            myInstance.SetActive(false);
+            Bullets.Add(myInstance);
+            myInstance.GetComponent<Bullet>().Damage = Damage;
+            myInstance.GetComponent<Bullet>().ID = RT_PNum.Substring(RT_PNum.Length-1);
+
+        }
+    }
+
+
+    void Update()
+    {
+        if (Input.GetAxisRaw(RT_PNum) > 0.5)
+        {
+            isShooting = true;
+        }
+        else
+        {
+            isShooting = false;
+
+            // Play shot smoke particles
+        }
+
+        if ((isShooting) && (t_RateOfFireTimer >= RateOfFire) && (BulletsInMag > 0))
+        {
+            //Then search through bullet list and fire the first inactive
+            for (int i = 0; i < BULLET_POOL_SIZE; i++)
+            {
+                if (Bullets[i].activeInHierarchy == false)
+                {
+                    // Play shot sound
+                    // Play shot anim
+                    // Play shot particles
+                    Bullets[i].SetActive(true);
+                    Bullets[i].transform.position = Emitter.position;
+                    // Recoil
+                    float randomAngle = Random.Range(Recoil, -Recoil);
+                    Bullets[i].transform.eulerAngles = gameObject.transform.parent.transform.eulerAngles - new Vector3(0, randomAngle, 0);
+                    BulletsInMag--;
+
+                    t_RateOfFireTimer = 0; // Reset ROF timer
+                    return;
+                }
+            }
+        }
+
+        if ((Input.GetButtonDown(xButton_PNum)) && (!requestReload))
+        {
+            // Play Reload Sound
+            // Also play a reload graphic on screen
+            tempReloading_Str = xButton_PNum;
+            print("This: " + tempReloading_Str);
+            requestReload = true;
+        }
+
+        if ((requestReload))
+        {
+            t_Reload += Time.deltaTime;
+
+            if(t_Reload > TimeToReload)
+            {
+                print("Reloaded GM: " + gameObject.transform.parent.gameObject.name);
+                BulletsInMag = MagazineCapacity;
+                requestReload = false;
+                t_Reload = 0;
+            }
+        }
+
+        t_RateOfFireTimer += Time.deltaTime;
+    }
+}
---
+++ Prototype/Assets/Scripts/WinDetection.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinDetection : MonoBehaviour {
+
+
+    private float health = 100F;
+    private Slider slider_PlayerHealth;
+
+    // Attach to the player.
+    private void Start()
+    {
+        if (gameObject.name == "Player Parent 1")
+        {
+            slider_PlayerHealth = GameObject.Find("Player 1 - Health").GetComponent<Slider>();
+        }
+        else if (gameObject.name == "Player Parent 2")
+        {
+            slider_PlayerHealth = GameObject.Find("Player 2 - Health").GetComponent<Slider>();
+        }
+        else if (gameObject.name == "Player Parent 3")
+        {
+            slider_PlayerHealth = GameObject.Find("Player 3 - Health").GetComponent<Slider>();
+        }
+        else if (gameObject.name == "Player Parent 4")
+        {
+            slider_PlayerHealth = GameObject.Find("Player 4 - Health").GetComponent<Slider>();
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // if this player collides with the bullet
+        if(other.gameObject.tag == "Bullet")
+        {
+            string temp = gameObject.name;
+            temp = temp.Substring(temp.Length-1);
+
+            print("TEMP VAR"+temp);
+
+            if(other.gameObject.GetComponent<Bullet>().ID != temp)
+            {
+                // This means our bullet came from another player
+                slider_PlayerHealth.value -= other.GetComponent<Bullet>().Damage;
+                print("<color=green>Player " + other.GetComponent<Bullet>().ID + " did " + other.GetComponent<Bullet>().Damage + " Damage</color>");
+
+                print("Made it here");
+                if (slider_PlayerHealth.value <= 0.2f)
+                    Destroy(gameObject);
+            }
+
+        }
+    }
+}
