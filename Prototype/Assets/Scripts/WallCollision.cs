58933e1c457788d16311209eaf023a5ec2d5050b
Mitchell Fukuzawa <mitchellfukuzawa@hotmail.com>
2018-09-20 16:35:06 -0400

    Player Menu

--- Prototype/Assets/Scripts/WallCollision.cs
+++ Prototype/Assets/Scripts/WallCollision.cs
@@ -4,26 +4,11 @@ using UnityEngine;
 
 public class WallCollision : MonoBehaviour {
 
-    public GameObject BulletDeath;
 
-
-
-    private void OnTriggerEnter(Collider collision)
+    private void OnTriggerEnter(Collider other)
     {
-        if (collision.gameObject.tag == "Bullet")
-        {
-            // Spawn System
-            GameObject particle1Instance = Instantiate<GameObject>(BulletDeath);
+        //print("COL: " + other.gameObject.name);
 
-            // Place System
-            particle1Instance.transform.position = (collision.transform.position);
-
-            // Set rotation
-            particle1Instance.transform.rotation = Quaternion.Euler(0,collision.transform.rotation.eulerAngles.y - 180,0);
-
-            // Delete bullet and set a destroy for system
-            collision.gameObject.SetActive(false);
-            Destroy(particle1Instance, 1);
-        }
+        other.gameObject.SetActive(false);
     }
 }
